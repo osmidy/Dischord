cb51a83b51b125375e5fd5c419d90490165282f5
Olivier S. Midy <osmidy@gmail.com>
2016-11-21 05:48:09 -0500

    THI

--- project/Environment.py
+++ project/Environment.py
@@ -13,7 +13,7 @@ from kivy.core.window import Window
 from kivy.clock import Clock as kivyClock
 from kivy.uix.label import Label
 from kivy.graphics.instructions import InstructionGroup
-from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.graphics import Color, Ellipse, Rectangle, Bezier
 from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
 from kivy.config import Config
 
@@ -29,14 +29,22 @@ class Environment(InstructionGroup):
         self.type = gtype
 
         if gtype == "foreground":
-       		self.color = Color(0.7, 0.6, 0.28)
-       		self.rect = Rectangle(pos=(0,100), size=(Window.width, 100))
+            self.color = Color(0.7, 0.6, 0.28)
+            self.outline = CEllipse(cpos=(Window.width / 2, -60), size = (1000, 360))
+            self.add(Color(.7, .6, .28))
+            self.add(self.outline)
+            
+            self.color = Color(.2, .6, .28)
+            self.circle = CEllipse(cpos=(Window.width / 2, -60), size = (900, 330))
+            
+            self.add(self.color)
+            self.add(self.circle)
+            
         elif gtype == "background":
             self.color = Color(0.2, 0.6, 0.28)
             self.rect = Rectangle(pos=(0,0), size=(Window.width, 500))
-
-        self.add(self.color)
-        self.add(self.rect)
+            self.add(self.color)
+            self.add(self.rect)
 
 
         
---
+++ project/Flame.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('..')
+from common.core import *
+from common.gfxutil import *
+from common.audio import *
+from common.mixer import *
+from common.note import *
+from common.wavegen import *
+from common.wavesrc import *
+from common.writer import *
+
+from kivy.core.window import Window
+from kivy.clock import Clock as kivyClock
+from kivy.uix.label import Label
+from kivy.graphics.instructions import InstructionGroup
+from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
+from kivy.config import Config
+
+from random import random, randint, choice
+import numpy as np
+
+import math
+
+
+class Flame(InstructionGroup):
+    def __init__(self, time, pos, speed):
+        super(Flame, self).__init__()
+        
+        # TODO: This will be a particle. Use circle for MS1
+        self.pos = pos
+        self.orignal_pos = pos
+        
+        # TODO: add sound note for contact
+        
+        self.color = Color(1, .65, 0) # Orange
+        self.add(self.color)
+        
+        self.circle = CEllipse(cpos = pos, size = (50, 50)) # radius = 50
+        
+        def on_update(self, dt):
+            pass
+            
+        def set_pos(self, pos):
+            self.pos = pos
+            self.circle.set_cpos(pos)
+            
+        def reset_pos(self):
+            self.pos = self.orignal_pos
+            self.circle.set_cpos(self.pos)
+            
+        def get_pos(self):
+            return self.pos
